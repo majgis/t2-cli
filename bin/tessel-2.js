ddcd427f1742323b09244d387bbae19887d20953
Michael Jackson <mjackson@overstock.com>
2016-04-16 14:13:10 -0600

    The basic setup of the command is working.

--- bin/tessel-2.js
+++ bin/tessel-2.js
@@ -133,6 +133,15 @@ parser.command('provision')
   })
   .help('Authorize your computer to control the USB-connected Tessel');
 
+parser.command('unbrick')
+  .callback(callControllerCallback('unbrickTessel'))
+  .option('openwrt', {
+    abbr: 'o',
+    flag: true,
+    help: 'Restore a factory version of OpenWrt'
+  })
+  .help('Unbrick your Tessel by restoring to factory settings.');
+
 makeCommand('restart')
   .callback(function(opts) {
     // 1. Check that the type is a valid type
